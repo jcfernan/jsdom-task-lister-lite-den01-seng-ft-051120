56c748afef75568c222c2f23dd922bddfd5406d1
Carlos Fernandez <jcfernan@live.com>
2021-01-10 16:56:12 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const task = document.querySelector('#list')
+const taskForm = document.querySelector('#create-task-form')
+
+
+document.addEventListener("submit", addTask);
+
+function addTask (event) {
+  event.preventDefault();
+  // console.log("form listener", event.target);
+
+  const taskFormData = new FormData(event.target);
+  const taskText = commentFormData.get("task");
+
+  const comment = document.createElement("li");
+  comment.textContent = commentText;
+  comments.append(comment);
+
+  event.target.reset();
+
+  // console.log("comment text", commentText);
+}
+
+
+
+
+
